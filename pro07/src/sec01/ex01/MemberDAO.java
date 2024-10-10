950d9e31afb1d9c0dacd69333047809be9fd2566
euneick <study.euneick@gmail.com>
2024-10-10 11:00:33 +0900

    DB의 table과 연결하여 쿼리문을 주고 받는 DAO 클래스 추가

---
+++ pro07/src/sec01/ex01/MemberDAO.java
@@ -0,0 +1,21 @@
+package sec01.ex01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 	DAO (Database Access Object) 클래스
+ * 		- DB의 table과 연결하여 쿼리문을 주고 받는 클래스 
+ */
+public class MemberDAO {
+
+	private void connectDB() {
+		
+	}
+	
+	public List<Object> getMembersList() {
+		List<Object> list = new ArrayList<Object>();
+		
+		return list;
+	}
+}
