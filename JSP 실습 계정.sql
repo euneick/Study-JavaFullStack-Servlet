78f61d77f7e4b95e3148dc98c5dc60b89693a15b
euneick <study.euneick@gmail.com>
2024-10-10 09:52:03 +0900

    DB 테이블 생성 및 값 추가 쿼리 작성

--- JSP 실습 계정.sql
+++ JSP 실습 계정.sql
@@ -3,5 +3,18 @@ create table t_member(
     pwd         varchar2(10),
     name        varchar2(50),
     email       varchar2(50),
-    joimnDate   date default sysdate
+    joinDate   date default sysdate
 );
+
+insert into t_member
+values('hong', '1212', 'È«±æµ¿', 'hong@gmail.com', sysdate);
+
+insert into t_member
+values('lee', '1212', 'ÀÌ¼ø½Å', 'lee@test.com', sysdate);
+
+insert into t_member
+values('kim', '1212', '±èÀ¯½Å', 'kim@jweb.com', sysdate);
+
+commit;
+
+select * from t_member;
