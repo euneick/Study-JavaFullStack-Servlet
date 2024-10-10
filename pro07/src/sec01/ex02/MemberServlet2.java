95a2cc69180909f0410f43c7e3c86ab91a30b475
euneick <study.euneick@gmail.com>
2024-10-10 15:05:13 +0900

    서블릿 클래스 이름 중복 해결

--- pro07/src/sec01/ex02/MemberServlet2.java
+++ pro07/src/sec01/ex02/MemberServlet2.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/member2")
-public class MemberServlet extends HttpServlet {
+public class MemberServlet2 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
