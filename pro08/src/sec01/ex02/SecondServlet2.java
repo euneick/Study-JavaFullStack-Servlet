d73a0f9ad7317953fbf5b9dc4ff275c535c9dd83
euneick <study.euneick@gmail.com>
2024-10-11 11:08:07 +0900

    refresh를 이용하여 1초 뒤 redirect 하는 포워드 예제 작성

---
+++ pro08/src/sec01/ex02/SecondServlet2.java
@@ -0,0 +1,28 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/second2")
+public class SecondServlet2 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter printWriter = response.getWriter();
+		
+		printWriter.print("<html><body>"
+				+ "sendRedirect를 이용한 redirect 실습"
+				+ "</body></html>");
+	}
+
+}
