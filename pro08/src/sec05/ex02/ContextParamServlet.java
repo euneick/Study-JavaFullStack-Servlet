d028cd80d6d566e2213c9f23d3bcfa645e894b0f
euneick <study.euneick@gmail.com>
2024-10-11 16:34:29 +0900

    오탈자 수정

--- pro08/src/sec05/ex02/ContextParamServlet.java
+++ pro08/src/sec05/ex02/ContextParamServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/initMenu")
+@WebServlet("/initParam")
 public class ContextParamServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
