9641017067e08d2e9e787149d713a52a5bfa958d
euneick <study.euneick@gmail.com>
2024-10-28 09:47:52 +0900

    member 하나에 대한 정보를 갖는 VO객체 생성

---
+++ CarProject/src/VOs/MemberVO.java
@@ -0,0 +1,115 @@
+package VOs;
+
+import java.sql.Date;
+
+public class MemberVO {
+	
+	private String id;
+	private String pass;
+	private String name;
+	private Date regDate;
+	private int age;
+	private String gender;
+	private String address;
+	private String email;
+	private String tel;
+	private String hp;
+	
+	public MemberVO() {
+	}
+
+	public MemberVO(String id, String pass, String name, Date regDate, int age, String gender, String address,
+			String email, String tel, String hp) {
+		
+		this.id = id;
+		this.pass = pass;
+		this.name = name;
+		this.regDate = regDate;
+		this.age = age;
+		this.gender = gender;
+		this.address = address;
+		this.email = email;
+		this.tel = tel;
+		this.hp = hp;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+}
