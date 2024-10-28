336ec2815183287f65b0f41f31a72b7ac1bb9431
euneick <study.euneick@gmail.com>
2024-10-28 09:51:46 +0900

    멤버와 관련된 DB작업을 하는 DAO 클래스 생성

---
+++ CarProject/src/DAOs/MemberDAO.java
@@ -0,0 +1,72 @@
+package DAOs;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	
+	private DataSource dataSource;
+	
+	private Connection connection;
+	private PreparedStatement statement;
+	private ResultSet resultSet;
+	
+	public MemberDAO() {
+		
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:/comp/env/jdbc/oracle");
+		}
+		catch (Exception e) {
+			System.out.println("DB 연결 실패");
+			e.printStackTrace();
+		}
+	}
+	
+	private void Release() {
+		
+		try {
+			if (resultSet != null) resultSet.close();
+			if (statement != null) statement.close();
+			if (connection != null) connection.close();
+		}
+		catch (Exception e) {
+			System.out.println("자원 해제 실패");
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
