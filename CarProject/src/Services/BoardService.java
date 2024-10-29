0acac530ca165e0bdf9feb6fd9767f8aff02ec0d
euneick <study.euneick@gmail.com>
2024-10-29 11:02:53 +0900

    게시판에서 멤버에 대한 정보와도 연결해야 하므로 멤버 데이터 접근 객체 생성

--- CarProject/src/Services/BoardService.java
+++ CarProject/src/Services/BoardService.java
@@ -1,13 +1,16 @@
 package Services;
 
 import DAOs.BoardDAO;
+import DAOs.MemberDAO;
 
 public class BoardService {
 
 	private BoardDAO boardDAO;
+	private MemberDAO memberDAO;
 	
 	public BoardService() {
 
 		boardDAO = new BoardDAO();
+		memberDAO = new MemberDAO();
 	}
 }
