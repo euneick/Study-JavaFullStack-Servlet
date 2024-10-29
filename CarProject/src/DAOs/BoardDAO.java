c43006cee8b24e8c4c53b2ed70b1e89a6c2e3fe5
euneick <study.euneick@gmail.com>
2024-10-29 10:57:41 +0900

    board DB table 과 연동 할 DAO 객체 생성

---
+++ CarProject/src/DAOs/BoardDAO.java
@@ -0,0 +1,43 @@
+package DAOs;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class BoardDAO {
+
+	private DataSource dataSource;
+	
+	private Connection connection;
+	private PreparedStatement statement;
+	private ResultSet resultSet;
+	
+	public BoardDAO() {
+
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:/comp/env/jdbc/oracle");
+		}
+		catch (Exception e) {
+			System.out.println("DB 연결 실패");
+			e.printStackTrace();
+		}
+	}
+	
+	private void Release() {
+		
+		try {
+			if (resultSet != null) resultSet.close();
+			if (statement != null) statement.close();
+			if (connection != null) connection.close();
+		}
+		catch (Exception e) {
+			System.out.println("자원 해제 실패");
+			e.printStackTrace();
+		}
+	}
+}
