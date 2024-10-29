149804c2d2dc9673ba2df02f8cb4c551050226d1
euneick <study.euneick@gmail.com>
2024-10-29 12:21:43 +0900

    board 테이블의 모든 데이터를 조회하는 구문 작성

--- CarProject/src/DAOs/BoardDAO.java
+++ CarProject/src/DAOs/BoardDAO.java
@@ -3,10 +3,13 @@ package DAOs;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import VOs.BoardVO;
 
 public class BoardDAO {
 
@@ -39,5 +42,46 @@ public class BoardDAO {
 			System.out.println("자원 해제 실패");
 			e.printStackTrace();
 		}
+	}
+	
+	public ArrayList<BoardVO> selectBoards() {
+		
+		ArrayList<BoardVO> boards = new ArrayList<BoardVO>();
+		
+		try {
+			connection = dataSource.getConnection();
+			
+			String sql = "select * from board order by b_group asc";
+			statement = connection.prepareStatement(sql);
+			
+			resultSet = statement.executeQuery();
+			
+			while (resultSet.next()) {
+				
+				BoardVO board = new BoardVO(
+						resultSet.getInt("b_idx"),
+						resultSet.getString("b_id"),
+						resultSet.getString("b_pw"),
+						resultSet.getString("b_name"),
+						resultSet.getString("b_email"),
+						resultSet.getString("b_title"),
+						resultSet.getString("b_content"),
+						resultSet.getInt("b_group"),
+						resultSet.getInt("b_level"),
+						resultSet.getDate("b_date"),
+						resultSet.getInt("b_cnt"));
+				
+				boards.add(board);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("DB 조회 실패");
+		}
+		finally {
+			Release();
+		}
+		
+		return boards;
 	}
 }
